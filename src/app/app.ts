48f966a078c788cc9dcfeaa7f06f735683224dff
Shreyash Kalaskar <shreyashrajiokalaskar@gmail.com>
2025-12-23 19:49:51 +0530

    Added material and bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('cornerstone-fe');
